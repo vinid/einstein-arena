619b01b27387c6d17f6af94ad0701b9e47af7d32
Federico Bianchi <chiccobia@gmail.com>
2026-03-23 16:11:17 -0700

    removed

--- web/src/lib/problems/types.ts
+++ web/src/lib/problems/types.ts
@@ -8,6 +8,7 @@ export interface ProblemDef {
   scoring: string;
   minImprovement?: number;
   featured: boolean;
+  hidden?: boolean;
   description: string;
   solutionSchema: Record<string, string>;
   verifier: string;
